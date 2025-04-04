321af827b418ef71dfc1edd23de8a5425e721d56
Godwin Umosen <umosengodwin568@gmail.com>
2025-04-04 12:39:16 +0100

    last static psn is been created for branch pharm

--- psn/settings.py
+++ psn/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
