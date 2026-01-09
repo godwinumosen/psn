f6d6a24850a6a0bdbb8a7fdb05a999545c5efd8e
Senwin.ng <godwinsenwin@outlook.com>
2026-01-09 22:19:56 +0100

    Topbar is been changed to icon in mobile

--- psn/settings.py
+++ psn/settings.py
@@ -138,7 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -146,7 +145,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'agilepharmdev@gmail.com'  # your Gmail address   
 EMAIL_HOST_PASSWORD = 'uvalikdhcxmifwhu'  # App password from Gmail godwinsenwin@gmail.com fyjncjgqwghgknwt
 DEFAULT_FROM_EMAIL = 'PSN Rivers <godwinsenwin@gmail.com>'
-
 
 LOGIN_URL = 'login'          # redirects non-authenticated users
 LOGIN_REDIRECT_URL = 'shop'  # where to go after login
