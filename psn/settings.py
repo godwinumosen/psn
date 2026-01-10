35e463bdf9a9f5ba9b422e27dab33cbf54fa11ea
Senwin.ng <godwinsenwin@outlook.com>
2026-01-10 07:02:40 +0100

    Topbar is been changed to icon in mobile

--- psn/settings.py
+++ psn/settings.py
@@ -144,7 +144,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'agilepharmdev@gmail.com'  # your Gmail address   
 EMAIL_HOST_PASSWORD = 'uvalikdhcxmifwhu'  # App password from Gmail godwinsenwin@gmail.com fyjncjgqwghgknwt
-DEFAULT_FROM_EMAIL = 'PSN Rivers <godwinsenwin@gmail.com>'
+DEFAULT_FROM_EMAIL = 'PSN Rivers <agilepharmdev@gmail.com>'
 
 LOGIN_URL = 'login'          # redirects non-authenticated users
 LOGIN_REDIRECT_URL = 'shop'  # where to go after login
